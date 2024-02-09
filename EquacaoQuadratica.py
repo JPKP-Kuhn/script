ba3a30ab3377cd3f5c68e2163eee77b79c261af9
João P. Kuhn <pedrojoaojpk@gmail.com>
2024-02-09 08:46:42 -0300

    Equação quadrática

---
+++ EquacaoQuadratica.py
@@ -0,0 +1,12 @@
+#Resolver equação do segundo grau
+print('Escreva a equação')
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
+
+delta = b**2 - 4 * a * c
+
+x1 = (-b + (delta)**(1/2)) / 2 * a
+x2 = (-b - (delta)**(1/2)) / 2 * a
+
+print(x1, x2)
