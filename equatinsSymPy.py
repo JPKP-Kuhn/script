6c93e43d30de6f2a2519a3ce159554855f2c8b62
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2024-05-22 22:50:56 -0300

    learning about simpy

--- equatinsSymPy.py
+++ equatinsSymPy.py
@@ -4,12 +4,19 @@ import math, sympy
 print(sympy.sqrt(17))
 print(math.sqrt(17))
 
-x = sympy.symbols('x')
+variable = sympy.symbols('x')
 y = sympy.symbols('y')
 
-expression = 2 * x * y
+expression = 2 * variable * y
 
 print(expression)
 print(sympy.frac(expression))
 
-print(sympy.solve(2*x + 3, x))
+a = 2
+b = 3
+c = 0
+print(sympy.solve(sympy.Eq(a*variable+b, c)))
+
+print(sympy.solve(sympy.Eq(2*variable + 3*variable + 4 - 2, variable)))
+
+print(sympy.solve(sympy.Eq(a*variable+b, c)))
